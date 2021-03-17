6f2208856cd522d5e892732ef7273b4151634974
Латыпова Диана Алексеевна <201713@mck-ktits.ru>
2021-03-17 18:56:13 +0300

    first

---
+++ Converter/Converter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    class Program
+    {
+        public static HandsClothesEntities2 db = new HandsClothesEntities2();
+        static void Main(string[] args)
+        {
+            foreach (var item in db.Materials)
+            {
+                if (item.Image!="")
+                {
+                    byte[] str = Encoding.Default.GetBytes(@"C:\Users\201713\Desktop\SecretRepozitory\КОД 1.1._ВАРИАНТ_4\Сессия 1" + item.Image);
+                    item.ByteImage = str;
+                    db.Materials.AddOrUpdate<Material>();
+
+                    //var bytes = File.ReadAllBytes(@"C:\Users\201713\Desktop\SecretRepozitory\КОД 1.1._ВАРИАНТ_4\Сессия 1" + item.Image);
+                    //db.Materials.AddOrUpdate<Material>();
+                }
+            }
+            db.SaveChanges();
+        }
+    }
+}
